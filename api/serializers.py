6338ae5cf437d17fb66839ae5a90aca8f9f798f4
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-17 20:41:23 +0200

    Created adding tags for posts, edited post serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from posts.models import Post, Group
+from posts.models import Post, Group, Tag, TagPost
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('name', )
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,7 +14,22 @@ class PostSerializer(serializers.ModelSerializer):
         slug_field='slug',
         required=False
     )
+    tag = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Post
-        fields = ('text', 'author', 'image', 'pub_date', 'group')
+        fields = ('text', 'author', 'image', 'pub_date', 'group', 'tag')
+
+    def create(self, validated_data):
+        if 'tag' not in validated_data:
+            post = Post.objects.create(**validated_data)
+            return post
+
+        tags = validated_data.pop('tag')
+        post = Post.objects.create(**validated_data)
+
+        for tag in tags:
+            current_tag, status = Tag.objects.get_or_create(**tag)
+            TagPost.objects.create(tag=current_tag, post=post)
+
+        return post
