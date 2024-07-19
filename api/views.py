baa3109b73cd499ffb88d7bc87809df6e82699bb
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-19 16:42:38 +0200

    Added new permission, only author can edit post

--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from posts.models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Post model objects.
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
+        """
+        Creates a post where the current user is the author.
+        """
         serializer.save(author=self.request.user)
