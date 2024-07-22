84b72325b2421a53a73df4464b0191901e94a8bf
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-22 10:45:09 +0200

    Create Docker and docker-compose files. Reconfigured settings to postgresql. Installed the required dependencies and added them to the file

--- jornaly_api/settings.py
+++ jornaly_api/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'jornaly_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
